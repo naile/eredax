9eff255d5dcbfef6f48ff2683e5fc9a42e99d1d5
Andreas Caravella <andreas.caravella@gmail.com>
2016-08-13 17:13:59 +0200

    azure compatible environment variable name

--- example.config.js
+++ example.config.js
@@ -4,6 +4,6 @@ config.apiKeys = {
     sl_realtid: "xxxxxxxx",
 }
 config.web = {};
-config.web.port = process.env.WEB_PORT || 8000;
+config.web.port = process.env.PORT || 8000;
 
 module.exports = config;
