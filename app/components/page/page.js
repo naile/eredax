3299553d8a300950b6785a7cd0eaa536a562d6f7
Andreas Caravella <andreas.caravella@gmail.com>
2016-08-13 14:28:16 +0200

    fix formatting

--- app/components/page/page.js
+++ app/components/page/page.js
@@ -2,20 +2,20 @@
 
 var pageService = angular.module('eredax.pageService', []);
 
-pageService.factory('Page', ['Config', function(Config) {
+pageService.factory('Page', ['Config', function (Config) {
   var title = 'foobar';
   return {
-    title: function() { return title; },
-    setTitle: function(newTitle) { title = newTitle; }
+    title: function () { return title; },
+    setTitle: function (newTitle) { title = newTitle; }
   };
 }]);
 
 angular.module('eredax.page', [])
 
-.controller('MainCtrl', ['$scope', 'Page', 'Config', function(sc, Page, Config) {
-  sc.Page = Page;
-  Config.getConfig().then(function(result) {
-    sc.Page.setTitle(result.title);
-  });
-  
-}]);
+  .controller('MainCtrl', ['$scope', 'Page', 'Config', function (sc, Page, Config) {
+    sc.Page = Page;
+    Config.getConfig().then(function (result) {
+      sc.Page.setTitle(result.title);
+    });
+
+  }]);
