da9b32420975e454315f71e9ac9ab14de3bd371f
Andreas Caravella <andreas.caravella@gmail.com>
2016-08-14 12:00:39 +0200

    put api url in conf and use a more flexible proxy implementation

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 var express = require('express'),
-  proxy = require('json-proxy'),
+  httpProxy = require('http-proxy'),
+  url = require('url'),
   conf = require('./config');
-var app = express()
+var app = express();
+var proxy = httpProxy.createProxyServer();
 
 
 app.use(express.static('app'));
 
-app.use(proxy.initialize({
-  proxy: {
-    'forward': {
-      '/api/(.*)': 'http://api.sl.se/api2/$1&key=' + conf.apiKeys.sl_realtid
-    }
-  }
-}));
+app.get('/api/realtimedepartures.json', function (req, res) {
+  var target = `${conf.slApiUrl}/realtimedepartures.json?${url.parse(req.url).query}&key=${conf.apiKeys.sl_realtid}`;
+  proxy.web(req, res, { target: target, ignorePath: true });
+});
 
 var server = app.listen(conf.web.port, function () {
 
