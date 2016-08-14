2fcfca3d0517c69d80134afa5f939b64167c684a
Andreas Caravella <andreas.caravella@gmail.com>
2016-08-14 14:26:59 +0200

    support for caching successful sl api responses

--- example.config.js
+++ example.config.js
@@ -4,6 +4,8 @@ config.apiKeys = {
     sl_realtid: "xxxxxxxx",
 }
 config.slApiUrl = 'http://api.sl.se/api2';
+config.cacheTtl = 10000; //ms
+config.enableCache = true;
 config.web = {};
 config.web.port = process.env.PORT || 8000;
 
--- server.js
+++ server.js
@@ -1,17 +1,37 @@
 var express = require('express'),
   httpProxy = require('http-proxy'),
+  cache = require('memory-cache'),
   url = require('url'),
   conf = require('./config');
 var app = express();
 var proxy = httpProxy.createProxyServer();
 
-
 app.use(express.static('app'));
 
 app.get('/api/realtimedepartures.json', function (req, res) {
-  var target = `${conf.slApiUrl}/realtimedepartures.json?${url.parse(req.url).query}&key=${conf.apiKeys.sl_realtid}`;
+  var query = url.parse(req.url).query;
+  var cached = cache.get(query)
+  if (cached) {
+    res.send(cached);
+    return;
+  }
+  var target = `${conf.slApiUrl}/realtimedepartures.json?${query}&key=${conf.apiKeys.sl_realtid}`;
   proxy.web(req, res, { target: target, ignorePath: true });
 });
+
+proxy.on('proxyRes', function (proxyRes, req, res) {
+  if (conf.enableCache && proxyRes.statusCode === 200)
+    cacheResponse(proxyRes, url.parse(req.url).query, conf.cacheTtl);
+});
+
+function cacheResponse(res, cacheKey, ttl) {
+  var body = [];
+  res.on('data', function (chunk) {
+    body.push(chunk)
+  }).on('end', function () {
+    cache.put(cacheKey, JSON.parse(Buffer.concat(body).toString()), ttl);
+  })
+}
 
 var server = app.listen(conf.web.port, function () {
 
